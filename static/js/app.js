1c18104270e1ef3bd845bbc7f302040e8f72cfda
Jennifer Gaddie <jennifergaddie@Jennifers-MacBook-Pro.local>
2021-07-22 21:52:39 -0400

    update to code, remove unneeded code, and final commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,118 +1,99 @@
 // Use d3 to read the data
-d3.json('samples.json', function (data) {
-    console.log(data) 
-})
-    // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-    //  Use `sample_values` as the values for the bar chart.
-    //  Use `otu_ids` as the labels for the bar chart.
-    //  Use `otu_labels` as the hovertext for the chart.
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+      buildGauge(result.wfreq);
+    });
+  }
 
-// Create an array for needed data 
-var sample_values = Object.sample_values
-var otu_ids = Object.otu_ids
-var otu_labels = Object.otu_labels
+//   define the variables needed to build the charts and put into an array; build filter at same time for the drop down
+    function buildCharts(sample) {
+        d3.json("samples.json").then((data) => {
+          var samples = data.samples;
+          var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+          var result = resultArray[0];
+          var otu_ids = result.otu_ids;
+          var otu_labels = result.otu_labels;
+          var sample_values = result.sample_values; 
+          var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: { t: 0 },
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30}
+          };
 
-// Create the Trace
-var trace1 = [{
-    x: sample_values,
-    y: otu_ids,
-    type: "bar",
-    orientation: "h"
-  }];
+// define the variable for the buble chart, as well as the mode which defines the bubble, ie mode, markers, size based on samples
+var bubbleData = [
+    {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }
+  ];
+//  call the bubble chart and place in the div on the index
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   
-  // Create the data array for the plot
-  var data = [trace1];
+//   Define the variables for the bar chart, as well at the type to prepare to the plot
+  var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+  var barData = [
+    {
+      y: yticks,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
+    }
+  ];
   
-  // Define the plot layout
-  var layout = {
-    title: {text: "Top 10 OTUs for Selected Individual",
-        font: {size: 24}},
-    xaxis: { title: "Sample Values" },
-    yaxis: { title: "OTUs" }
+//   define bar layout, including title, and margins
+  var barLayout = {
+    title: "Top 10 Bacteria Cultures Found",
+    margin: { t: 30, l: 150 }
   };
-  
-  // Plot the chart to a div tag with id "bar"
-  Plotly.newPlot("bar", data, layout);
 
-
-// 3. Create a bubble chart that displays each sample.
-    //  Use `otu_ids` for the x values.
-    //  Use `sample_values` for the y values.
-    //  Use `sample_values` for the marker size.
-    //  Use `otu_ids` for the marker colors.
-    //  Use `otu_labels` for the text values.
-
-    var trace1 = {
-        x: [1, 2, 3, 4],
-        y: [10, 11, 12, 13],
-        text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-        mode: 'markers',
-        marker: {
-          size: [400, 600, 800, 1000],
-          sizemode: 'area'
-        }
-      };
-      
-      var trace2 = {
-        x: [1, 2, 3, 4],
-        y: [14, 15, 16, 17],
-        text: ['A</br>size: 40</br>sixeref: 0.2', 'B</br>size: 60</br>sixeref: 0.2', 'C</br>size: 80</br>sixeref: 0.2', 'D</br>size: 100</br>sixeref: 0.2'],
-        mode: 'markers',
-        marker: {
-          size: [400, 600, 800, 1000],
-          //setting 'sizeref' to lower than 1 decreases the rendered size
-          sizeref: 2,
-          sizemode: 'area'
-        }
-      };
-      
-      var trace3 = {
-        x: [1, 2, 3, 4],
-        y: [20, 21, 22, 23],
-        text: ['A</br>size: 40</br>sixeref: 2', 'B</br>size: 60</br>sixeref: 2', 'C</br>size: 80</br>sixeref: 2', 'D</br>size: 100</br>sixeref: 2'],
-        mode: 'markers',
-        marker: {
-          size: [400, 600, 800, 1000],
-          //setting 'sizeref' to less than 1, increases the rendered marker sizes
-          sizeref: 0.2,
-          sizemode: 'area'
-        }
-      };
-      
-      // sizeref using above forumla
-      var desired_maximum_marker_size = 40;
-      var size = [400, 600, 800, 1000];
-      var trace4 = {
-        x: [1, 2, 3, 4],
-        y: [26, 27, 28, 29],
-        text: ['A</br>size: 40</br>sixeref: 1.25', 'B</br>size: 60</br>sixeref: 1.25', 'C</br>size: 80</br>sixeref: 1.25', 'D</br>size: 100</br>sixeref: 1.25'],
-        mode: 'markers',
-        marker: {
-          size: size,
-          //set 'sizeref' to an 'ideal' size given by the formula sizeref = 2. * max(array_of_size_values) / (desired_maximum_marker_size ** 2)
-          sizeref: 2.0 * Math.max(...size) / (desired_maximum_marker_size**2),
-          sizemode: 'area'
-        }
-      };
-      
-      var data = [trace1, trace2, trace3, trace4];
-      
-      var layout = {
-        title: 'Bacteria Cultures Per Sample ',
-        showlegend: false,
-        xaxis: { title: "Sample Values" },
-        yaxis: { title: "OTU ID" },
-        height: 600,
-        width: 1200
-      };
-      
-      Plotly.newPlot('bubble', data, layout);    
-
-// 4. Display the sample metadata, i.e., an individual's demographic information.
-
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
-
-// 6. Update all of the plots any time that a new sample is selected. 
-
-
+  //   call bar plot and put into bar div on html
+  Plotly.newPlot("bar", barData, barLayout);
+});
+}
  
+// Display the sample metadata, i.e., an individual's demographic information.
+
+// Display each key-value pair from the metadata JSON object somewhere on the page.
+
+// Update all of the plots any time that a new sample is selected. 
+
+function init() {
+    var selector = d3.select("#selDataset");
+    d3.json("samples.json").then((data) => {
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+      var firstSample = sampleNames[0];
+      buildCharts(firstSample);
+      buildMetadata(firstSample);
+    });
+  }
+  function optionChanged(newSample) {
+    buildCharts(newSample);
+    buildMetadata(newSample);
+  }
+  init();
+
